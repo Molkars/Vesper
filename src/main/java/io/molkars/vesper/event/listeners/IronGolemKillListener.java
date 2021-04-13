2b725451a500d6aae3d27aa831663369008b01d7
Dillon Shaffer <d.j.shaffer@outlook.com>
2021-04-12 19:42:41 -0500

    Remove the possibility of getting 16 random flower drops

--- src/main/java/io/molkars/vesper/event/listeners/IronGolemKillListener.java
+++ src/main/java/io/molkars/vesper/event/listeners/IronGolemKillListener.java
@@ -7,9 +7,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class IronGolemKillListener implements EventsListener {
   @Override
   public void onEntityDeath(EntityDeathEvent e) {
@@ -30,17 +27,10 @@ public class IronGolemKillListener implements EventsListener {
     e.getDrops().clear();
 
     final int nuggetCount = 3 * ingotCount;
-
     final Vesper vesper = Vesper.getInstance();
-    List<ItemStack> flowers = new ArrayList<>(poppyCount);
     for (int i = 0; i < poppyCount; i++) {
-      ItemStack flower = kFlowers[vesper.random.nextInt(kFlowers.length)];
-      final int index = flowers.indexOf(flower);
-      if (index < 0) {
-        flowers.add(flower);
-      } else {
-        flower = flowers.get(index);
-        flower.setAmount(flower.getAmount() + 1);
+      if (vesper.random.nextDouble() < (1 / 6942f)) {
+        e.getDrops().add(new ItemStack(Material.IRON_BLOCK, 1));
       }
     }
 
@@ -48,23 +38,4 @@ public class IronGolemKillListener implements EventsListener {
       e.getDrops().add(new ItemStack(Material.IRON_NUGGET, nuggetCount));
     }
   }
-
-  static final ItemStack[] kFlowers = new ItemStack[]{
-      new ItemStack(Material.DANDELION),
-      new ItemStack(Material.POPPY),
-      new ItemStack(Material.BLUE_ORCHID),
-      new ItemStack(Material.ALLIUM),
-      new ItemStack(Material.AZURE_BLUET),
-      new ItemStack(Material.RED_TULIP),
-      new ItemStack(Material.ORANGE_TULIP),
-      new ItemStack(Material.WHITE_TULIP),
-      new ItemStack(Material.PINK_TULIP),
-      new ItemStack(Material.OXEYE_DAISY),
-      new ItemStack(Material.CORNFLOWER),
-      new ItemStack(Material.LILY_OF_THE_VALLEY),
-      new ItemStack(Material.SUNFLOWER),
-      new ItemStack(Material.ROSE_BUSH),
-      new ItemStack(Material.LILAC),
-      new ItemStack(Material.PEONY),
-  };
 }
